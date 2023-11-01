a168765f08c9e95a40799242c409690c5783eb5c
신산하 <tksgk2598@naver.com>
2023-11-02 00:33:31 +0900

    Solved: Greedy

---
+++ BaekJoon/Silver/S2_11501_주식/S2_11501_주식_신산하.java
@@ -0,0 +1,33 @@
+//메모리 : 320872KB
+//시간 : 4516ms
+
+import java.util.Scanner;
+
+public class S2_11501_주식_신산하 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int T=1;T<=t;T++){
+            int day = sc.nextInt();
+            long[] fee = new long[day];
+            for(int i=0;i<day;i++){
+                fee[i] = sc.nextInt();
+            }
+    
+            //1. 맨 끝 날짜를 max로 시작
+            long max = fee[day-1];
+    
+            //2. 끝에서부터 돌면서 max보다 작으면 이득에 더해주고, max보다 크면 max를 교체한다.
+            long result = 0;
+            for(int d=day-1;d>=0;d--){
+                if(fee[d]<=max) result += max-fee[d];
+                else max = fee[d];
+            }
+            
+            System.out.println(result);
+        }
+    }
+}
