2f69b9188b92f4a6cf13e08fe086557eb6662e3f
ParkSeYun98 <yun12343@naver.com>
2023-11-06 09:00:09 +0900

    Solved: 이분탐색

--- BaekJoon/Silver/S2_2512_예산/S2_2512_예산_박세윤.java
+++ BaekJoon/Silver/S2_2512_예산/S2_2512_예산_박세윤.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class S2_2512_예산 {
+public class S2_2512_예산_박세윤 {
 
     private static int N;
     private static int M;
