9748bc80e1c2ecffb4e7909d4f344047f353f635
신산하 <tksgk2598@naver.com>
2023-11-28 00:35:13 +0900

    Solved: PriorityQueue

---
+++ BaekJoon/Silver/S1_2841_외계인의_기타_연주/S1_2841_외계인의_기타_연주_신산하.java
@@ -0,0 +1,79 @@
+/*
+* Memory : 383356 KB
+* Time : 1588 ms
+* */
+
+import java.util.Scanner;
+import java.util.PriorityQueue;
+
+public class S1_2841_외계인의_기타_연주_신산하 {
+
+    public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();//5
+		int P = sc.nextInt();//15
+		
+		PriorityQueue<Node>[] touchList = new PriorityQueue[N];
+		
+		for(int i=0;i<N;i++){
+			touchList[i] = new PriorityQueue<>();
+		}
+		
+		//test case
+		int result = 0; 
+
+		for(int T=0;T<N;T++){
+			int curLine = sc.nextInt();
+			int curFlat = sc.nextInt();
+			
+			if(touchList[curLine].isEmpty()){//해당 라인의 터치가 없다면
+				touchList[curLine].add(new Node(curLine, curFlat));
+				result++;
+			}else{//터치가 있다면
+
+				Node peek = touchList[curLine].peek();
+
+				
+				//라인에 현재 터치보다 큰 플랫이 있다면
+				while (peek!=null && peek.flat>curFlat) {
+					//손떼기
+					touchList[curLine].poll();
+					result++;
+					peek = touchList[curLine].peek();
+				}
+
+				//손 댈 수 있는 상황 만들고,
+
+				//내가 누르려고 하는 터치와 플랫이 같다면 그냥 넘기기
+				if(peek!=null && peek.flat==curFlat) continue;
+
+				//작거나 아예 없다면 그냥 넣어도 됨
+				//손대기
+				touchList[curLine].add(new Node(curLine, curFlat));
+				result++;
+
+			}			
+			
+		}//for end
+
+		System.out.println(result);
+		
+	}
+	
+	public static class Node implements Comparable<Node>{
+		int line;
+		int flat;
+		
+		public Node(int line, int flat){
+			this.line = line;
+			this.flat = flat;
+		}
+		
+		@Override
+		public int compareTo(Node o){
+			return this.flat<o.flat?1:-1;
+		}
+	}
+}
