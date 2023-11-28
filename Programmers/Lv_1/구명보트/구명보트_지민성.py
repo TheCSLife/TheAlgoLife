56ff8ed0530e002618c7b9cbe2ec3444c0a01c99
지민성 <jiminsung@naver.com>
2023-11-28 22:34:30 +0900

    Solved: Greedy

---
+++ Programmers/Lv_1/구명보트/구명보트_지민성.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    people = deque(people)
+    while people:
+        if len(people) >= 2:
+            if people[0] + people[-1] <= limit:
+                people.pop()
+                people.popleft()
+                answer = answer + 1
+            else:
+                people.pop()
+                answer = answer + 1
+        else:
+            answer = answer + 1
+            break
+    return answer
