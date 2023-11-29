c07c6edb9c6a50c328a6d0f7617529bb51daf44c
신산하 <tksgk2598@naver.com>
2023-11-29 13:59:33 +0900

    Solved: DP

---
+++ BaekJoon/Silver/S3_2579_계단_오르기/S3_2579_계단_오르기_신산하.java
@@ -0,0 +1,40 @@
+/*
+* Memory : 11596 KB
+* Time : 80 ms
+* */
+
+package BaekJoon.Silver;
+import java.io.*;
+
+public class S3_2579_계단_오르기_신산하 {
+
+    static int[] dp;
+    static int[] floor_list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int floor_count = Integer.parseInt(br.readLine());
+
+        floor_list = new int[floor_count];
+        for(int i=0;i<floor_count;i++){
+            int score = Integer.parseInt(br.readLine());
+            floor_list[i] = score;
+        }
+
+        dp = new int[floor_count];
+        dp[0] = floor_list[0];
+        if(floor_count>=2) dp[1] = floor_list[0]+floor_list[1];
+
+        if(floor_count>=3) dp[2] = Math.max(floor_list[0], floor_list[1])+floor_list[2];
+        
+        System.out.println(find(floor_count-1));
+    }
+
+    public static int find(int N){ //N까지의 최대값을 계산
+
+        if(dp[N]==0)
+            dp[N] = Math.max(floor_list[N-1]+find(N-3), find(N-2))+floor_list[N];
+        
+        return dp[N];
+
+    }
+}
