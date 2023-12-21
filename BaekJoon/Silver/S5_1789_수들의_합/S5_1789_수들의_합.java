1628bfdafe9c46b8811316a6ecab3ab78e2ee454
SeYun <81186461+ParkSeYun98@users.noreply.github.com>
2023-12-21 17:03:03 +0900

    Solved: Greedy

---
+++ BaekJoon/Silver/S5_1789_수들의_합/S5_1789_수들의_합.java
@@ -0,0 +1,33 @@
+/*
+* Memory : 11532 KB
+* Time : 80 ms
+* */
+
+package BaekJoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S5_1789_수들의_합 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long S = Long.parseLong(br.readLine());
+        long sum = 0;
+        int idx = 1;
+        int cnt = 0;
+
+        while(true) {
+            if(sum > S)
+                break;
+            else {
+                sum += idx++;
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt - 1);
+    }
+}
