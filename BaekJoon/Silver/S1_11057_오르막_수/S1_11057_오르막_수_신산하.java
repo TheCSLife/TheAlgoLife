55d61cc31bdb292d3d9cffa5d066ed0e38527f5a
신산하 <tksgk2598@naver.com>
2024-01-24 12:27:28 +0900

    Solved: Dynamic Programming

---
+++ BaekJoon/Silver/S1_11057_오르막_수/S1_11057_오르막_수_신산하.java
@@ -0,0 +1,31 @@
+/*
+* Memory : 11568 KB
+* Time : 80 ms
+* */
+
+package BaekJoon.Silver;
+
+import java.util.*;
+import java.io.*;
+
+public class S1_11057_오르막_수_신산하 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine()); //자릿수 최대 1000자리
+
+    int[][] dp = new int[N+1][10]; //행 : 자릿수, 열 : 0~9
+    Arrays.fill(dp[0],1);
+    
+    for(int r=1;r<=N;r++){
+        for(int c=0; c<=9; c++){
+           for(int k=0;k<=c;k++){
+               dp[r][c] += dp[r-1][k];
+               dp[r][c] %= 10007;
+           }
+        }
+       
+    }
+    
+    System.out.println(dp[N][9] % 10007);
+  }
+}
