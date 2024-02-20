02dac822367906d547f73387feb6935259a3ca2a
신산하 <tksgk2598@naver.com>
2024-02-20 12:16:30 +0900

    Solved: Dynamic Programming

---
+++ BaekJoon/Silver/S1_1309_동물원/S1_1309_동물원_신산하.java
@@ -0,0 +1,28 @@
+/*
+* Memory : 14724 KB
+* Time : 88 ms
+* */
+
+package BaekJoon.Silver;
+
+import java.io.*;
+
+public class S1_1309_동물원_신산하 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+
+    int dp[][] = new int[N][2];
+    dp[0][0] = 2;
+    dp[0][1] = 3;
+
+    for(int r=1; r<N; r++){
+        for(int c=0; c<2; c++){
+            if(c==0) dp[r][c] = (dp[r-1][0] + dp[r-1][1])%9901;
+            if(c==1) dp[r][c] = (dp[r-1][0] + dp[r][0])%9901;
+        }
+    }
+
+    System.out.println(dp[N-1][1]);
+  }
+}
