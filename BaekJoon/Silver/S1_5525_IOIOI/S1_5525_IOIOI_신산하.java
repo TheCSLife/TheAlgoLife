24f7db4c45b02e00138217007203fdab9fe8cf7c
신산하 <tksgk2598@naver.com>
2024-02-22 12:11:48 +0900

    Solved: Sliding Window

---
+++ BaekJoon/Silver/S1_5525_IOIOI/S1_5525_IOIOI_신산하.java
@@ -0,0 +1,45 @@
+/*
+* Memory : 20504 KB
+* Time : 184 ms
+* */
+
+package BaekJoon.Silver;
+
+import java.io.*;
+
+class S1_5525_IOIOI_신산하 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int M = Integer.parseInt(br.readLine());
+    String line = br.readLine();
+
+    char target = 'I';
+    int count = 0;
+    int result = 0;
+    for (int i = 0; i < M; i++) {
+      if (line.charAt(i) == target) {
+        count++;
+        if (target == 'I')
+          target = 'O';
+        else if (target == 'O')
+          target = 'I';
+
+        if (count == N + N + 1) {
+          result++;
+          count -= 2;
+        }
+      } else {
+        if (line.charAt(i) == 'I') {
+          target = 'O';
+          count = 1;
+        } else {
+          target = 'I';
+          count = 0;
+        }
+      }
+    }
+
+    System.out.println(result);
+  }
+}
